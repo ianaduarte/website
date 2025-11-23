0277f499de21529623e107f730bd71ebd7bbd20c
ianaduarte <118121781+ianaduarte@users.noreply.github.com>
2025-11-23 11:01:29 -0400

    Start using scss

---
+++ sass_compile.sh
@@ -0,0 +1,7 @@
+find . -name "*.scss" -print0 | while IFS= read -r -d $'\0' scss_file; do
+    output_dir=$(dirname "$scss_file")
+    base_name=$(basename "$scss_file" .scss)
+    css_file="$output_dir/$base_name.css"
+    
+    "sass" "$scss_file" "$css_file"
+done
